cc282d7480f08192108392ce236c019639b76e30
Iván Camilo Barragán Echavarría <80495143+AtodeKimeru@users.noreply.github.com>
2024-03-22 12:25:02 +0000

    test & issue #1

--- app/turn_request/tests.py
+++ app/turn_request/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import MyModel
 
 # Create your tests here.
+class TestTurnRequest(TestCase):
+    def test_turn_request(self):
+        self.assertEqual(1, 1)
+
+
+class MyModelTest(TestCase):
+    def setUp(self):
+        # Set up data for the tests
+        MyModel.objects.create(name="test")
+
+    def test_name(self):
+        # Test the name field
+        model = MyModel.objects.get(name="test")
+        self.assertEqual(model.name, "test")
